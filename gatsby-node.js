4f82f18956e313fb65e636ebfc3346060489c31c
webstellar <pete.onyegbule@gmail.com>
2022-03-21 15:55:09 +0100

    Fifth Commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,15 @@
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  createPage({
+  /*   createPage({
     path: "/using-dsg",
-    //component: require.resolve("./src/templates/using-dsg.js"),
+    component: require.resolve("./src/templates/using-dsg.js"),
+    context: {},
+    defer: false,
+  }) */
+
+  createPage({
+    path: "/Layout",
+    component: require.resolve("./src/components/Layout/Layout.js"),
     context: {},
     defer: false,
   })
